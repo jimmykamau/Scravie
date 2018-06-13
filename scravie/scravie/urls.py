e5d5e7dfb0542993a6dee10fc741d9f6d39196c0
Jimmy Kamau <jimmy@beamimpact.com>
2018-06-14 00:24:17 +0300

    Register front_end urls

--- scravie/scravie/urls.py
+++ scravie/scravie/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('scravie.api.urls'))
+    path('api/', include('scravie.api.urls')),
+    path('movie/', include('scravie.front_end.urls'))
 ]
