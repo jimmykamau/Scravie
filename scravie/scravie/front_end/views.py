6eac4595ec728b5e684003cf734ebcb94b63b74a
Jimmy Kamau <jimmy@beamimpact.com>
2018-06-14 00:23:20 +0300

    Create IndexView

--- scravie/scravie/front_end/views.py
+++ scravie/scravie/front_end/views.py
@@ -1,3 +1,15 @@
+import requests
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    def get(self, request):
+        movies_api_url = request.build_absolute_uri(reverse('movie-api'))
+        movies_data = requests.get(
+            movies_api_url,
+            headers={"Content-Type": "application/json"}
+        )
+        context = dict(movies=movies_data.json())
+        return render(request, 'index.html', context)
