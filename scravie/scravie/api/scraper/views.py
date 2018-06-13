5b74b8cd84dc4850c672f95d16d497fe297885a0
Jimmy Kamau <jimmy@beamimpact.com>
2018-06-13 18:58:24 +0300

    Create ListMovieView endpoint

--- scravie/scravie/api/scraper/views.py
+++ scravie/scravie/api/scraper/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+import scravie.api.scraper.models as scraper_models
+import scravie.api.scraper.serializers as scraper_serializers
+
+
+class ListMovieView(generics.ListAPIView):
+    permission_classes = (permissions.AllowAny,)
+    queryset = scraper_models.Movie.objects.all()
+    serializer_class = scraper_serializers.MovieSerializer
