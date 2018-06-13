3606d0c7ef0e489dfd1c267dd740920834a2f115
Jimmy Kamau <jimmy@beamimpact.com>
2018-06-14 02:14:12 +0300

    Add movie-detail url

--- scravie/scravie/front_end/urls.py
+++ scravie/scravie/front_end/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 import scravie.front_end.views as front_end_views
 
 urlpatterns = [
-    path('', front_end_views.IndexView.as_view(), name='movies')
+    path('', front_end_views.IndexView.as_view(), name='movies'),
+    path('<slug:id>/', front_end_views.DetailView.as_view(), name='movie-detail')
 ]
