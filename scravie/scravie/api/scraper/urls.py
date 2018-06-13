023d71941eee6fb3715b4223b5644c5ec9715fd5
Jimmy Kamau <jimmy@beamimpact.com>
2018-06-13 20:24:25 +0300

    Add ListMovieDetailView

--- scravie/scravie/api/scraper/urls.py
+++ scravie/scravie/api/scraper/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from scravie.api.scraper import views
 
 urlpatterns = [
-    path('', views.ListMovieView.as_view(), name='movie-api')
+    path('', views.ListMovieView.as_view(), name='movie-api'),
+    path('<slug:id>/', views.ListMovieDetailView.as_view(), name='movie-detail-api')
 ]
