63f8823c81cc9e9c0e56f6cb78a7c9aee7597cc6
Jimmy Kamau <jimmy@beamimpact.com>
2018-06-14 02:14:25 +0300

    Add movie-detail view

--- scravie/scravie/front_end/views.py
+++ scravie/scravie/front_end/views.py
@@ -13,3 +13,16 @@ class IndexView(TemplateView):
         )
         context = dict(movies=movies_data.json())
         return render(request, 'index.html', context)
+
+
+class DetailView(TemplateView):
+    def get(self, request, id):
+        movie_detail_api_url = '{}?id={}'.format(
+            request.build_absolute_uri(reverse('movie-api')),
+            id)
+        movie_detail_data = requests.get(
+            movie_detail_api_url,
+            headers={"Content-Type": "application/json"}
+        )
+        context = dict(movie=movie_detail_data.json()[0])
+        return render(request, 'detail.html', context)
