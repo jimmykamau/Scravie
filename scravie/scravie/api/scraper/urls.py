ad4be235a44dc3c7e70883e6ca19d8dfadf24044
Jimmy Kamau <jimmy@beamimpact.com>
2018-06-28 22:43:49 +0300

    Add sort URL

--- scravie/scravie/api/scraper/urls.py
+++ scravie/scravie/api/scraper/urls.py
@@ -5,5 +5,6 @@ from scravie.api.scraper import views
 urlpatterns = [
     path('', views.ListMovieView.as_view(), name='movie-api'),
     path('search/', views.ListMovieSearchView.as_view(), name='search-api-view'),
+    path('sort/', views.ListMovieSortView.as_view(), name='sort-api-view'),
     path('<slug:id>/', views.ListMovieDetailView.as_view(), name='movie-detail-api')
 ]
