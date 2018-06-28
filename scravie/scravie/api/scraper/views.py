79634ac37157f54cce8cd8457821636b721aa26f
Jimmy Kamau <jimmy@beamimpact.com>
2018-06-28 17:33:13 +0300

    Add ListMovieSearch view

--- scravie/scravie/api/scraper/views.py
+++ scravie/scravie/api/scraper/views.py
@@ -1,5 +1,8 @@
 from rest_framework import generics, permissions
 from django_filters.rest_framework import DjangoFilterBackend
+import django.contrib.postgres.search as postgres_search
+import functools
+import operator
 
 import scravie.api.scraper.models as scraper_models
 import scravie.api.scraper.serializers as scraper_serializers
@@ -20,3 +23,20 @@ class ListMovieDetailView(generics.ListAPIView):
     def get_queryset(self):
         movie_id = self.kwargs['id']
         return scraper_models.MovieDetail.objects.filter(movie__id=movie_id)
+
+
+class ListMovieSearchView(generics.ListAPIView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = scraper_serializers.MovieSerializer
+
+    def get_queryset(self):
+        queryset = scraper_models.Movie.objects.all()
+        search_string = self.request.query_params.get('name', None)
+        if search_string is not None:
+            terms = [postgres_search.SearchQuery(term) for term in search_string.split()]
+            vector = postgres_search.SearchVector('name')
+            query = functools.reduce(operator.or_, terms)
+            queryset = scraper_models.Movie.objects.annotate(
+                rank=postgres_search.SearchRank(vector, query)
+            ).order_by('-rank').filter(rank__gte=0.06)
+        return queryset
