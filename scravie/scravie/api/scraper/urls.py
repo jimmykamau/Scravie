caf17730c62a16817a020de3d2b8cb3c86b0452c
Jimmy Kamau <jimmy@beamimpact.com>
2018-06-28 17:33:01 +0300

    Add search URL

--- scravie/scravie/api/scraper/urls.py
+++ scravie/scravie/api/scraper/urls.py
@@ -4,5 +4,6 @@ from scravie.api.scraper import views
 
 urlpatterns = [
     path('', views.ListMovieView.as_view(), name='movie-api'),
+    path('search/', views.ListMovieSearchView.as_view(), name='search-api-view'),
     path('<slug:id>/', views.ListMovieDetailView.as_view(), name='movie-detail-api')
 ]
