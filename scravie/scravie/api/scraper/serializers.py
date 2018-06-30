a8b88b97711cc724cabc75b529dc716a4550172d
Jimmy Kamau <jimmy@beamimpact.com>
2018-06-30 17:16:21 +0300

    Add support for times showing

--- scravie/scravie/api/scraper/serializers.py
+++ scravie/scravie/api/scraper/serializers.py
@@ -19,21 +19,31 @@ class MovieDetailSerializer(serializers.ModelSerializer):
                   'actors', 'directors', 'synopsis')
 
 
+class TimesShowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = scraper_models.TimesShowing
+        fields = ('time_showing',)
+
+
 class MovieSerializer(serializers.ModelSerializer):
     movie_details = MovieDetailSerializer(many=True)
+    times_showing = TimesShowingSerializer(many=True)
 
     class Meta:
         model = scraper_models.Movie
         fields = ('id', 'name', 'details_url', 'thumbnail_url',
-                  'duration', 'days_showing', 'time_showing', 'movie_details')
+                  'duration', 'days_showing', 'time_showing',
+                  'movie_details', 'times_showing')
 
     def create(self, validated_data):
         movie_details_data = validated_data.pop('movie_details')
+        times_showing = validated_data.pop('times_showing')
         movie = scraper_models.Movie.objects.create(**validated_data)
         for detail in movie_details_data:
             actors = detail.pop('actors')
             directors = detail.pop('directors')
-            instance = scraper_models.MovieDetail.objects.create(movie=movie, **detail)
+            instance = scraper_models.MovieDetail.objects.create(
+                movie=movie, **detail)
             actors_model_list = []
             for actor in actors:
                 actor_model, created = scraper_models.Person.objects.get_or_create(
@@ -46,4 +56,6 @@ class MovieSerializer(serializers.ModelSerializer):
                 directors_model_list.append(director_model)
             instance.actors.set(actors_model_list)
             instance.directors.set(directors_model_list)
+        for time in times_showing:
+            scraper_models.TimesShowing.objects.create(movie=movie, **time)
         return movie
