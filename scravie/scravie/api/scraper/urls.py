21f840991e9f30583950405061532e602d23bf95
Jimmy Kamau <jimmy@beamimpact.com>
2018-06-30 17:17:18 +0300

    Add sort-date-api-view url

--- scravie/scravie/api/scraper/urls.py
+++ scravie/scravie/api/scraper/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
 from scravie.api.scraper import views
 
 urlpatterns = [
     path('', views.ListMovieView.as_view(), name='movie-api'),
     path('search/', views.ListMovieSearchView.as_view(), name='search-api-view'),
     path('sort/', views.ListMovieSortView.as_view(), name='sort-api-view'),
+    path('date/<slug:date>/', views.ListMovieSortDatetimeView.as_view(),
+         name='sort-date-api-view'),
     path('<slug:id>/', views.ListMovieDetailView.as_view(), name='movie-detail-api')
 ]
