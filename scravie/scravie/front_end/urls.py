46a8930b9d8d32c2994b59841c8a8c5d8b00a6d6
Jimmy Kamau <jimmy@beamimpact.com>
2018-07-01 16:02:00 +0300

    Add front-end urls

--- scravie/scravie/front_end/urls.py
+++ scravie/scravie/front_end/urls.py
@@ -4,5 +4,7 @@ import scravie.front_end.views as front_end_views
 
 urlpatterns = [
     path('', front_end_views.IndexView.as_view(), name='movies'),
-    path('<slug:id>/', front_end_views.DetailView.as_view(), name='movie-detail')
+    path('date-filter/', front_end_views.SearchDateView.as_view(), name='date-filter'),
+    path('sort-by-name/', front_end_views.SortNameView.as_view(), name='sort-by-name'),
+    path('movie-detail/<slug:id>/', front_end_views.DetailView.as_view(), name='movie-detail')
 ]
